5656c940ac22b626537bd5581059dec5fa59fc9f
fantaz-st <cbabic@pfst.hr>
2025-11-12 00:27:20 +0100

    added not-found page

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <main style={{ padding: 24 }}>
+          <h1>Page not found</h1>
+          <p>We couldn’t find that page.</p>
+        </main>
+      </body>
+    </html>
+  );
+}
